4f4a79bb23e2eb87215e401d0aa08f82671ed39f
CaptainPirate12 <lorenzmarlbusito04@gmail.com>
2026-07-10 16:49:03 +0800

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,16 +1,16 @@
-// Main app
-function togglePassword(){
+function togglePassword() {
 
-    const password=document.getElementById("password");
+    const password = document.getElementById("password");
+    const icon = document.querySelector(".eye-btn i");
 
-    if(password.type==="password"){
-
-        password.type="text";
-
-    }else{
-
-        password.type="password";
-
+    if (password.type === "password") {
+        password.type = "text";
+        icon.classList.remove("fa-eye");
+        icon.classList.add("fa-eye-slash");
+    } else {
+        password.type = "password";
+        icon.classList.remove("fa-eye-slash");
+        icon.classList.add("fa-eye");
     }
 
 }

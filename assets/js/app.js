eb836b72cea402d8b9a4f74b84fe52d05944a453
CaptainPirate12 <lorenzmarlbusito04@gmail.com>
2026-07-10 16:39:26 +0800

    Implement password visibility toggle function
    Added a function to toggle password visibility.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,16 @@
 // Main app
+function togglePassword(){
+
+    const password=document.getElementById("password");
+
+    if(password.type==="password"){
+
+        password.type="text";
+
+    }else{
+
+        password.type="password";
+
+    }
+
+}
